7c08ca2dcfa40115c376f37a7a2f450a69f9c91b
Marwan Mohamed <marawanmohamed9876@gmail.com>
2024-06-06 12:18:35 +0300

    feat: added routes of iframe and mainpage componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxChessBoardModule } from 'ngx-chess-board';
-
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { IframepageComponent } from './iframepage/iframepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    IframepageComponent
   ],
   imports: [
     BrowserModule,
